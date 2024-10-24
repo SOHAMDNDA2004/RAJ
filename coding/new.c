a7a5c36f7134055ffd77986abea95c0ec4a59964
SOHAMDNDA2004 <sohamdinda893@gmail.com>
2024-10-24 19:04:39 +0530

    Add files via upload

---
+++ coding/new.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ #include<stdlib.h>
+ #include<string.h>
+ struct Employee
+ {
+   char Ename[50];
+   int Ecode;
+   float Esal;
+   char Eph no[15];
+ };
+ int compareSalary(const void *a,const void *b)
+ {
+   
+ }
